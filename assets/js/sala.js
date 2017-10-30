7ed59f89c8b3b4b31761c812eba003df5b142a89
Adriel Barbosa <adrielbb15@hotmail.com>
2017-10-29 23:23:34 -0200

    Atulizando interface Sala

--- assets/js/sala.js
+++ assets/js/sala.js
@@ -1,19 +1,53 @@
+$(function($) {
+  $(".btnBoxSend").val("");
+});
+$("#msgArquivo").toggleClass("invisivel");
+$("#notice").toggleClass("invisivel");
+
 $("#send").click(function() {
-  var text = $(".btnBoxSend").val();
-  var nome = "Adriel";
-  var data = criaData();
-  var span = '<span class="NomeEData">' + nome + " - " + data + "</span>";
-  var p = '<div class="divParagrafo">' + text + "</div>";
-  var post = '<div class="postModel">' + span + p + "</div>";
-  $("#posts").prepend(post);
+    var text = $(".btnBoxSend").val();
+
+    if (text != "") {
+        var nome = "Adriel";
+        var data = criaData();
+        var span = '<span class="NomeEData">' + nome + " - " + data + "</span>";
+        var p = '<div class="divParagrafo">'+'<span>' + text +'<span>' + "</div>";
+        var post = '<div class="postModel">' + span + p + "</div>";
+        $(".btnBoxSend").val("");
+        $("#posts").prepend(post);
+        $("#notice").stop().fadeOut();
+
+    } else {
+  
+        $("#notice").css({"background-color": "#e74848"});
+        $("#notice").text("ERRO: Digita alguma coisa carai");
+        $("#notice").stop().fadeIn();
+
+    }
 });
 
 function criaData() {
-  var data = new Date();
- 
-  var hora = data.getHours();
-  var min = data.getMinutes();
-  var seg = data.getSeconds();
-  var str_hora = hora + ":" + min + ":" + seg;
-  return str_hora;
+    var data = new Date();
+
+    var hora = data.getHours();
+    var min = data.getMinutes();
+    var seg = data.getSeconds();
+    var str_hora = hora + ":" + min;
+    return str_hora;
 }
+
+$("#upload").change(function() {
+    var dir = $("#upload").val();
+    var msg = "Arquivo Selecionado: " + dir;
+
+    // $(".fa-times").toggle();
+    // $(".fa-upload").toggle();
+
+    $("#arquivoReport").text(msg);
+    $("#msgArquivo").fadeIn();
+});
+
+$(".iCloseFile").click(function() {
+    $("#msgArquivo").fadeOut();
+    $("#upload").val("");
+});
